1868707d76d1a6237f0b2d1c2176d83d13664023
Mirajul <islammirajul150978@gmail.com>
2024-11-03 13:07:38 +0600

    books poka

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Book from '../Book/Book';
 
 const Books = () => {
       
@@ -8,13 +9,18 @@ const Books = () => {
             fetch('./booksData.json')
             .then(Response => Response.json())
             .then(data => setBooks(data))
-      },[])
+      },[]);
 
 
       return (
             <div>
-                  <h2 className="text-5xl">Books</h2>
-                  <p>{books.length}</p>
+                  <h2 className="text-5xl text-center my-5 font-bold">Books</h2>
+                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                  {
+                        books.map(book => <Book book={book} key={book.bookId}></Book>)
+                  }
+                 </div>
+
             </div>
       );
 };
